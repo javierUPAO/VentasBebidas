0785a8fd86c30a9d038b925ab5d01ed97457217f
javierUPAO <jaguilarb3@upao.edu.pe>
2026-01-23 14:29:19 -0500

    fix/back: corrigiendo logica para consumir credenciales

--- back/firestoreConection.js
+++ back/firestoreConection.js
@@ -1,6 +1,18 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./creds/serviceAccountKey.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+} else {
+  serviceAccount = require("./creds/serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

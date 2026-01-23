134170720a0e7fedd29979803fce9ea5a36268d8
javierUPAO <jaguilarb3@upao.edu.pe>
2026-01-23 14:39:49 -0500

    fix:Configuracion de despliegue

--- back/firestoreConection.js
+++ back/firestoreConection.js
@@ -3,13 +3,11 @@ var admin = require("firebase-admin");
 let serviceAccount;
 
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-  });
+  serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  };
 } else {
   serviceAccount = require("./creds/serviceAccountKey.json");
 }
